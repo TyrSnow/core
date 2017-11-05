5456f6f5a36784f830d2a3a25c9136a2428ac9fa
Yu Tian <1127114376@qq.com>
2017-11-05 20:54:06 +0800

    add simple unit test

---
+++ src/test/api/tools/util.test.ts
@@ -0,0 +1,37 @@
+import {
+    generate_sault,
+    hash_password,
+    valid_password
+} from '../../../api/tools/util'
+
+import { expect, assert } from 'chai'
+import 'mocha'
+
+describe('Test generate_sault', () => {
+    it('should return a hex number string, and it\'s length is bigger or equal 64. ', () => {
+        const result = generate_sault();
+        assert.isString(result);
+        assert.match(result, /^[\da-fA-F]{64,}$/);
+    })
+
+    it('should return a string with seed string before it when a seed argument passed into.', () => {
+        const result = generate_sault('test');
+        assert.isString(result);
+        assert.match(result, /^test[\da-fA-F]{60,}$/);
+    })
+
+    it('should return normally when called it 10000 in 100ms.', () => {
+        let arr = [];
+        let timeStart = new Date().getMilliseconds();
+        for (let i = 0; i < 10000; i++) {
+            arr.push(generate_sault());
+        }
+        let timeEnd = new Date().getMilliseconds();
+        expect(arr.length).equal(10000);
+        expect(timeEnd - timeStart).lessThan(100);
+    })
+})
+
+describe('Test hash_password', () => {
+    
+})
