723baae38d99c3b3aa2b0c3723af6d961bcebb3b
Yu Tian <1127114376@qq.com>
2018-08-18 23:32:08 +0800

    return empty array when folder not exist

--- util/walk.ts
+++ util/walk.ts
@@ -3,16 +3,18 @@ import * as path from 'path';
 
 function walk(folder: string) {
   let filepaths = [];
-  const paths = fs.readdirSync(folder);
-  paths.map(file => {
-    const filepath = path.join(process.cwd(), folder, file);
-    const stat = fs.lstatSync(filepath);
-    if (stat.isDirectory()) {
-      filepaths = filepaths.concat(walk(filepath));
-    } else {
-      filepaths.push(filepath);
-    }
-  });
+  if (fs.existsSync(folder)) {
+    const paths = fs.readdirSync(folder);
+    paths.map(file => {
+      const filepath = path.join(process.cwd(), folder, file);
+      const stat = fs.lstatSync(filepath);
+      if (stat.isDirectory()) {
+        filepaths = filepaths.concat(walk(filepath));
+      } else {
+        filepaths.push(filepath);
+      }
+    });
+  }
 
   return filepaths;
 }
